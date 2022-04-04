6157ff49330a35bc530b0f311b4816cc2047db74
Kalamkar <skalamkar1@FVFZMYB5J1WK.tld>
2022-04-04 13:31:37 -0500

    game finished

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ var wins = 0
 usernumber.focus();
 
 function run() {
-    document.getElementById("paragraph").innerHTML = "Hello World!";
-    document.getElementById("paragraph").style.backgroundColor = "red";
-    document.getElementById("paragraph").style.color = "blue";
+    // document.getElementById("paragraph").innerHTML = "Hello World!";
+    // document.getElementById("paragraph").style.backgroundColor = "red";
+    // document.getElementById("paragraph").style.color = "blue";
     // rn()
     // userNum()
     compare()
@@ -22,30 +22,30 @@ function run() {
 
 function rn() {
     var ran = Math.floor(Math.random() * 10 + 1)
-    var x = document.getElementById("random")
+    // var x = document.getElementById("random")
 
-    x.style.color="white"
-    x.style.backgroundColor="blue"
-    x.style.padding="20px"
-    x.style.textAlign="center"
+    // x.style.color="white"
+    // x.style.backgroundColor="blue"
+    // x.style.padding="20px"
+    // x.style.textAlign="center"
 
-    document.getElementById("random").innerHTML = ran;
-    document.getElementById("random").style.backgroundColor = "red";
-    document.getElementById("random").style.color = "white";
+    // document.getElementById("random").innerHTML = ran;
+    // document.getElementById("random").style.backgroundColor = "red";
+    // document.getElementById("random").style.color = "white";
     return ran
 }
 
 function userNum() {
     var user = document.getElementById("getNumber").value;
     // alert(user)
-    var y = document.getElementById("userNumber")
+    // var y = document.getElementById("userNumber")
 
-    y.style.color="white"
-    y.style.backgroundColor="green"
-    y.style.padding="20px"
-    y.style.textAlign="center"
+    // y.style.color="white"
+    // y.style.backgroundColor="green"
+    // y.style.padding="20px"
+    // y.style.textAlign="center"
 
-    document.getElementById("userNumber").innerHTML = user
+    // document.getElementById("userNumber").innerHTML = user
     return user
 }
 
@@ -61,26 +61,30 @@ function compare() {
     z.style.padding="20px"
     z.style.textAlign="center"
 
-    c.style.color="white"
-    c.style.backgroundColor="orange"
-    c.style.padding="20px"
-    c.style.textAlign="center"
-
-
-    w.style.color="white"
-    w.style.backgroundColor="black"
-    w.style.padding="20px"
-    w.style.textAlign="center"
-
-
+    
+    
+    
+    
     if (a != b) {
         z.innerHTML = `Your number is not the same! The computer said ${b} and you said ${a}.`
         counter++
         c.innerHTML = `You have tried ${counter} times.`
+        c.style.color="white"
+        c.style.backgroundColor="orange"
+        c.style.padding="20px"
+        c.style.textAlign="center"
+        document.querySelector("body").style.backgroundColor = "#ff9c9c"
     } else if (a==b){
+        z.style.backgroundColor="lime"
         z.innerHTML = `Your number was the same!`
         wins++
         w.innerHTML = `You have won ${wins} times.`
+        w.style.color="white"
+        w.style.backgroundColor="black"
+        w.style.padding="20px"
+        w.style.textAlign="center"
+        counter = 0
+        document.querySelector("body").style.backgroundColor = "#e4ff9c"
     }
     reset()
 }
